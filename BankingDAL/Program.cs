b3517381f67890d960fb1822ad6daf51fd35fa17
Niranjan <niranjanhari123@gmail.com>
2021-09-15 17:40:27 +0530

    Version1

---
+++ BankingDAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingDAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BusinessLayerBankingObject/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BusinessLayerBankingObject
+{
+    public class Class1
+    {
+    }
+}
